06faf910c6a27b25ac8fd24dbfe334e05c49acde
Khajappa Biradar <khajappa.biradar.28.2@mountblue.tech>
2024-07-03 18:14:00 +0530

    added api end poins

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 
-const pool = require("./app/models/db");
-
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.send("Visit api/tutorial");
+  response.send([
+    {"api/tutorials" : "get all Tutorials"},
+    {"api/tutorials/:id" : "get Tutorial by id"},
+    {"api/tutorials" : "add new Tutorial(This is post method)"},
+    {"api/tutorials/:id" : "update Tutorial by id"},
+    {"api/tutorials/:id" : "remove Tutorial by id"},
+    {"api/tutorials" : "remove all Tutorials (This is delete method)"},
+    {"api/tutorials/published" : "find all published Tutorials"},
+    {"api/tutorials?title=[kw]" : "find all Tutorials which title contains 'kw' "}
+  ]);
 });
 
 const tutorialRouter = require("./app/routes/tutorial.routes");
